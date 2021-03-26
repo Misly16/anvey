aa342ce33b380e86ce51113b1f2224f802001715
Misly <60405462+Misly16@users.noreply.github.com>
2021-03-26 17:16:10 +0000

    fix schema

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 
-  _id: mongoose.Schema.Types.ObjectId,
   username: String,
   password: String,
   admin: Boolean,
