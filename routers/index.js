048f4344c6d5dc9db73392611b8fa5dc12d9bc7e
Misly <60405462+Misly16@users.noreply.github.com>
2021-03-27 11:10:53 +0000

    add a way to register

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 module.exports = function(express, passport) {
   const User = require('../db/models/user');
+  const bcrypt = require('bcrypt');
   const router = express.Router();
 
   router.get('/', function(req, res) {
@@ -16,13 +17,33 @@ module.exports = function(express, passport) {
     });
   });
 
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+  });
+
   router.post('/auth', passport.authenticate('local', {failureRedirect: '/login'}), function(req, res) {
     res.redirect('/dashboard');
   });
 
-  router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/login');
+  router.post('/register', function(req, res) {
+    User.findOne({admin: true}, function(err, doc) {
+      if (err) throw new Error(err);
+      if (doc) return res.status(403).send('Inactive route');
+      if (!req.body || !req.body[0] || !req.body[1]) return res.status(400).send('Incorrect body');
+      bcrypt.hash(req.body[1].value, 14, function(err, hash) {
+        if (err) throw new Error(err);
+        new User({
+          username: req.body[0].value,
+          password: hash,
+          admin: true,
+        }).save();
+      });
+      // Prevents it from asking the user to register again
+      setTimeout(function() {
+        res.redirect('/login');
+      }, 1000);
+    });
   });
   return router;
 };
