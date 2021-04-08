f9fe504810093f742291673e7196168621be2dca
Misly <60405462+Misly16@users.noreply.github.com>
2021-04-08 16:42:36 +0100

    added the forms view

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -6,6 +6,10 @@ module.exports = function(express, passport) {
     res.render('dashboard.html', {companyName: process.env.COMPANY_NAME, user: req.user});
   });
 
+  router.get('/forms', checkAuth, function(req, res) {
+    res.render('forms.html', {companyName: process.env.COMPANY_NAME, user: req.user});
+  });
+
   function checkAuth(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect('/login');
