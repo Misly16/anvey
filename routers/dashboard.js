1cf4d6f461c6780c6dc028a0d942db5340a4c173
Misly <60405462+Misly16@users.noreply.github.com>
2021-04-08 22:25:13 +0100

    routers for forms and creation

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
 module.exports = function(express, passport) {
+  const Survey = require('../db/models/survey');
+  const {nanoid} = require('nanoid');
   const router = express.Router();
 
   router.get('/', checkAuth, function(req, res) {
@@ -7,7 +9,24 @@ module.exports = function(express, passport) {
   });
 
   router.get('/forms', checkAuth, function(req, res) {
-    res.render('forms.html', {companyName: process.env.COMPANY_NAME, user: req.user});
+    Survey.find({}, function(err, doc) {
+      if (err) throw new Error(err);
+      res.render('forms.html', {companyName: process.env.COMPANY_NAME, user: req.user, forms: doc});
+    });
+  });
+
+  router.get('/forms/new', checkAuth, function(req, res) {
+    res.render('new-form.html', {companyName: process.env.COMPANY_NAME, user: req.user});
+  });
+
+  router.post('/forms/new', checkAuth, function(req, res) {
+    if (!req.body || !req.body[0] || !req.body[1] || req.body[0].value === '' || req.body[1].value === '') return res.status(400).send('Incorrect body');
+    new Survey({
+      id: nanoid(10),
+      title: req.body[0].value,
+      description: req.body[1].value,
+    }).save();
+    res.redirect('/dashboard/forms');
   });
 
   function checkAuth(req, res, next) {
