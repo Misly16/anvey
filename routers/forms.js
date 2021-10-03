c0e0952696a3423f34d46fee7c5277266fcb01c7
Misly <60405462+Misly16@users.noreply.github.com>
2021-10-03 12:25:20 +0100

    lint

--- routers/forms.js
+++ routers/forms.js
@@ -1,21 +1,14 @@
 module.exports = function(express, passport) {
-    const Survey = require('../db/models/form');
-    const {nanoid} = require('nanoid');
-    const router = express.Router();
-  
-    router.get('/:id', function(req, res) {
-        Survey.findOne({id: req.params.id}, function(err, doc) {
-            if (err) throw new Error(err);
-            if (doc) return res.render('forms/index.html', {companyName: process.env.COMPANY_NAME, user: req.user, form: doc});
-            res.status(404).send('This form does not exist')
-        });
-    });
+  const Survey = require('../db/models/form');
+  const router = express.Router();
 
-    function checkAuth(req, res, next) {
-      if (req.isAuthenticated()) return next();
-      res.redirect('/login');
-    }
-  
-    return router;
-  };
-  
+  router.get('/:id', function(req, res) {
+    Survey.findOne({id: req.params.id}, function(err, doc) {
+      if (err) throw new Error(err);
+      if (doc) return res.render('forms/index.html', {companyName: process.env.COMPANY_NAME, user: req.user, form: doc});
+      res.status(404).send('This form does not exist');
+    });
+  });
+
+  return router;
+};
